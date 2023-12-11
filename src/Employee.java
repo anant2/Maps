bf65cbdd4fc03e76f309c8d5b4669545ef61d967
Anant Majhi <anmajhi7@gmail.com>
2023-12-11 22:05:34 +0530

    sorting logic added

---
+++ src/Employee.java
@@ -0,0 +1,42 @@
+public class Employee implements Comparable<Employee>{
+
+    public Long id;
+    public String name;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee obj) {
+        return (int) (this.id - obj.id);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Employee> map = new HashMap<>();
+        Employee employee1 = new Employee(1L, "Mher");
+        map.put(employee1.getName(), employee1);
+        Employee employee2 = new Employee(22L, "Annie");
+        map.put(employee2.getName(), employee2);
+        Employee employee3 = new Employee(8L, "John");
+        map.put(employee3.getName(), employee3);
+        Employee employee4 = new Employee(2L, "George");
+        map.put(employee4.getName(), employee4);
+
+        TreeMap<String, Employee> sorted = new TreeMap<>();
+        sorted.putAll(map);
+
+        map.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+
+
+    }
+}
